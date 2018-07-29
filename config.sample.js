86982bc8898e6342beb31000e50478b9f3f95817
Adrian Mullings <adralemechk@gmail.com>
2018-07-29 01:20:47 -0400

    Discord support, and improved event summaries

--- config.sample.js
+++ config.sample.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   TOKEN_PATH: path.join(__dirname, 'token'),
-  SLACK_WEBHOOK: 'https://hooks.slack.com/services/...',
+  WEBHOOK_URL: 'https://hooks.slack.com/services/...',
+  WEBHOOK_CONTENT_FIELD: 'text',
   NUCLINO_APP_ID: '...',
   NUCLINO_BRAIN_ID: '...',
   NUCLINO_TEAM: 'MyTeamName',
--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const fs = require('fs');
 const ShareDB = require('@danielbuechele/sharedb/lib/client');
 const {
   TOKEN_PATH,
-  SLACK_WEBHOOK,
+  WEBHOOK_URL,
+  WEBHOOK_CONTENT_FIELD,
   NUCLINO_APP_ID,
   NUCLINO_BRAIN_ID,
   NUCLINO_TEAM,
 } = require('./config.js');
+
+const NOTIFICATION_DELAY = 30 * 1000;
 
 function getHeaders(token) {
   return {
@@ -69,49 +72,145 @@ function subscribeBrain(connection) {
 
   brain.subscribe();
   brain.on('load', () => {
-    console.log(brain.data.mainCellId);
+    console.log('brain:', brain.data);
     traverseTree(connection, brain.data.mainCellId);
   });
 }
 
 const cells = {};
-function subscribeCell(connection, name, cb) {
-  console.log('subscribe ' + name);
-  const cell = connection.get('ot_cell', name);
+function subscribeCell(connection, id, cb) {
+  const cell = connection.get('ot_cell', id);
   cell.subscribe();
-  cells[name] = cell;
-  cell.on('op', () => triggerUpdate(connection, cell));
+  console.log(`subscribed to cell ${id}`);
+  cells[id] = cell;
+  cell.on('op', (ops, source) => {
+    console.log('op', cell.id, ops);
+    scheduleNotification(cell, ops[0]);
+  });
   if (typeof cb === 'function') {
     cell.on('load', () => cb(cell));
   }
 }
 
-const timers = {};
-function triggerUpdate(connection, cell) {
-  // check for new cells
-  for (let i = 0; i < cell.data.childIds.length; i++) {
-    if (!visited.has(cell.data.childIds[i])) {
-      console.log(`new page created ${cell.id}`);
-      subscribeCell(connection, cell.data.childIds[i]);
-      return;
-    }
+const pending = {};
+function scheduleNotification(cell, op) {
+  const id = getTargetId(cell, op);
+  let newCell = false;
+  if (!cells[id]) {
+    newCell = true;
+    subscribeCell(cell.connection, id);
   }
 
-  console.log(`updated cell ${cell.id}`);
-  if (timers[cell.id]) {
-    clearTimeout(timers[cell.id]);
+  const summary = getSummary(cell, op);
+
+  let update = {};
+  if (pending[id]) {
+    update = pending[id];
+    update.summaries.push(summary);
+    clearTimeout(update.timeout);
+  } else {
+    update.cellId = id;
+    update.summaries = [summary];
+    update.newCell = newCell;
   }
-  timers[cell.id] = setTimeout(() => {
-    fetch(SLACK_WEBHOOK, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        text: `<https://app.nuclino.com/${NUCLINO_TEAM}/General/${cell.id}|${
-          cell.data.title
-        }> wurde aktualisiert`,
-      }),
-    });
-  }, 2 * 60 * 1000);
+
+  update.timeout = setTimeout(() => {
+    notify(update);
+    clearTimeout(update.timeout);
+    delete pending[id];
+  }, NOTIFICATION_DELAY);
+  pending[id] = update;
+}
+
+function getTargetId(cell, op) {
+  const field = op.p[0];
+  if (field === 'childIds' && op.li != null) {
+    return op.li;
+  }
+  if (field === 'childIds' && op.ld != null) {
+    return op.ld;
+  }
+  if (field === 'childIds' && op.lm != null) {
+    return cell.data.childIds[op.lm];
+  }
+  return cell.id;
+}
+
+function getSummary(cell, op) {
+  const field = op.p[0];
+  if (field === 'childIds' && op.li != null) {
+    const parent = getCellData(cell.id)
+    return `Added to cluster \`${parent.name}\``;
+  }
+  if (field === 'childIds' && op.ld != null) {
+    const parent = getCellData(cell.id)
+    return `Removed from cluster \`${parent.name}\``;
+  }
+  if (field === 'childIds' && op.lm != null) {
+    const from = op.p[1];
+    const to = op.lm;
+    const parent = getCellData(cell.id)
+    return `Moved from position \`${from}\` to \`${to}\` in cluster \`${parent.name}\``;
+  }
+  if (field === 'memberIds' && op.li != null) {
+    return `Added member`;
+  }
+  if (field === 'memberIds' && op.ld != null) {
+    return `Removed member`;
+  }
+  if (field === 'title') {
+    return `Title changed`;
+  }
+  if (field === 'updatedAt') {
+    return `Content changed`;
+  }
+  return `Changed \`${field}\``;
+}
+
+function notify(update) {
+  const message = getUpdateDesc(update);
+  let body = {};
+  body[WEBHOOK_CONTENT_FIELD] = message
+  fetch(WEBHOOK_URL, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(body),
+  })
+  .then((resp) => {
+    if (!resp.ok) {
+      console.error(resp);
+    }
+  })
+}
+
+function getUpdateDesc({ cellId, summaries, newCell }) {
+  const data = getCellData(cellId);
+  summaries = Array.from(new Set(summaries));
+  summaries = summaries.map((str) => `- ${str}`);
+  const desc = [
+    `${data.type} ${newCell ? 'created' : 'updated'}:\n\`${data.name}\``,
+    ...summaries,
+    `<${data.link}>`,
+  ];
+  return desc.join('\n');
+}
+
+function getCellData(id) {
+  const kindMapping = {
+    'PARENT': 'Cluster',
+    'LEAF': 'Item',
+  };
+  let doc = cells[id];
+  return {
+    id: id,
+    name: doc.data.title || 'Untitled',
+    type: kindMapping[doc.data.kind],
+    link: `https://app.nuclino.com/${NUCLINO_TEAM}/General/${id}`,
+  };
+}
+
+function removeDuplicates(array) {
+  return Array.from(new Set(array));
 }
 
 let killProcess = null;
